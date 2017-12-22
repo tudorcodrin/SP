8b82fa1514e0e7c3b3dcf97488546743173ecb49
tudorcodrin <31468472+tudorcodrin@users.noreply.github.com>
2017-12-22 13:01:34 +0200

    Update Tema_2_cu_cc.m

--- Tema 2 SP/Tema_2_cu_cc.m
+++ Tema 2 SP/Tema_2_cu_cc.m
@@ -9,6 +9,7 @@ N=50;
 D=3;
 q=D/T;
 k=1:N;
+t=0:0.01:T;         %definirea unui vector de timp
 w=2*pi/T;
 Xk=(1/T)*int(exp(-j*k*w*timp),timp,0,3);	%calculul direct al coeficientilor din forma exponentiala
 Ak=2*abs(Xk);					%calculul amplitudinilor
@@ -24,9 +25,9 @@ end
 
     Amp=[P,q,Ak]			%realizarea vectorului amplitudine
 
-stem(n,Amp)			%reprezentarea spectrului de amplitudini, cu componenta continua
 
-t=0:0.01:T;         %definirea unui vector de timp
+stem(n,Amp)			%reprezentarea spectrului de amplitudini, cu componenta continua
+title('Spectrul lui x(t)');
 
 x=q;            %semnalului i se atribuie valoarea componentei continue q*A=q, deoarece A=1
 for m=1:50          %reconstruirea semnalului
@@ -35,5 +36,5 @@ end
 
 plot(t,0.5*square(2*pi*t/T,7.5)+0.5);       %trasarea graficului semnalului initial
 hold on;
-plot(t,x,'--')                       %trasarea graficului semnalului reconstruit
-
+plot(t,x,'--');                       %trasarea graficului semnalului reconstruit
+title('x(t) (linie solida) si reconstructia folosind N coeficienti (linie punctata)')
